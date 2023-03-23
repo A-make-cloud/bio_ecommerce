d8717eb587ccb4f2ee4b5672c4e4e6b29af7b994
DavidJuricic <david.juricic@hotmail.fr>
2023-03-23 09:41:22 +0100

    modif  produits et add to cart

---
+++ frontend/src/components/products/AddCartButton.js
@@ -0,0 +1,9 @@
+
+function AddCartButton({ productId }) {
+  return (
+    <div style={{}}>
+        <button>Add to cart</button>
+    </div>
+  );
+}
+export default AddCartButton;
--- frontend/src/components/products/Card.js
+++ frontend/src/components/products/Card.js
@@ -1,12 +1,13 @@
 //import css from './css.css';
-//import AddCardBtn from './AddCardBtn';
+import AddCartButton from './AddCartButton';
 
 function Card({ product }) {
   return (
     <div style={{width: "300px"}}>
         <h4>{product.name +' '+ product.id}</h4>
         <p>{product.description.slice(0, 40)+"..."}</p>
-        <p>{product.price}</p>
+        <p>{product.price_ht}</p>
+        <AddCartButton productId={product.id}/>
     </div>
   );
 }
