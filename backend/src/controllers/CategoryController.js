2af6d4585d1b6f64d7d6abf5f28036ee7ff1ed08
DavidJuricic <david.juricic@hotmail.fr>
2023-03-30 12:46:28 +0200

    toto

--- backend/src/controllers/CategoryController.js
+++ backend/src/controllers/CategoryController.js
@@ -3,7 +3,7 @@ const { Category } = require('../../models');
 exports.findAll = (req, res) => {
     Category.findAll()
         .then(data => {
-            res.send(data);
+            res.sataus(201).send({data});
         })
         .catch(err => {
             res.status(500).send({
