f6cd864a1ecf17c40501e233eca7ac8002ec0d31
A-make-cloud <kessag75@yahoo.com>
2023-04-05 14:56:45 +0200

    ajout run buid

--- frontend/src/components/layouts/TopCategorie.js
+++ frontend/src/components/layouts/TopCategorie.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
-
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import { useParams } from "react-router-dom";
+import Categories from "../products/Categories";
 import CategorieTop from "./CategorieTop";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -21,9 +29,28 @@ const div1Style = {
 
 
 function TopCategorie() {
+    const theme = useTheme();
 
+    const params = useParams()
+    const categId = params.id;
     const [categories, setCategories] = useState([]);
 
+    // useEffect(function effectFunction() {
+    //     async function fetchCateg() {
+    //         const response = await fetch('/categories/findAll');
+    //         console.log(response.status)
+
+    //         if (response.status === 201) {
+    //             const json = await response.json();
+
+    //             console.log(json)
+    //             setCategories(json.data)
+    //         } else {
+    //             console.log("no categ")
+    //         }
+    //     }
+    //     fetchCateg();
+    // }, []);
 
     useEffect(() => {
 
@@ -32,7 +59,7 @@ function TopCategorie() {
             await fetch('/categories/findAll')
                 .then(response => response.json())
                 .then((res) => {
-                    //console.log("categorie donnée :");
+                    console.log(res.data);
                     setCategories(res.data)
                 })
         }
@@ -43,7 +70,139 @@ function TopCategorie() {
     return (
         <div style={div1Style}>
             <center><h2 >Top catégorie</h2></center>
+            {/* <Box sx={{ flexGrow: 1, marginTop: 5, marginBottom: 20 }}>
+                <Grid container spacing={2}>
 
+                    <Grid xs={4}>
+
+                        <Item>
+                            {categories && categories.map((cat, index) => (
+                                <Card sx={{ display: 'flex' }}>
+                                    <Box sx={{ display: 'flex', flexDirection: 'column', background: `${cat.background}` }}>
+                                        <CardContent sx={{ flex: '1 0 auto' }}>
+                                            <Typography component="div" variant="h5">
+                                                <p>{cat.title}</p>
+                                                <h5 >
+                                                    <br /> {cat.description.substring(1, 20)}</h5>
+                                            </Typography>
+
+                                        </CardContent>
+
+                                    </Box>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ width: 151 }}
+                                        image={cat.img ? cat.img.url : 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e'}
+                                        alt={cat.img ? cat.img[0].title : 'pas d image'}
+
+
+                                    />
+                                </Card>
+                            ))}
+                        </Item>
+
+                    </Grid>
+
+
+                    <Grid xs={4}>
+                        <Item>
+                            <Card sx={{ display: 'flex' }}>
+                                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                    <CardContent sx={{ flex: '1 0 auto' }}>
+                                        <Typography component="div" variant="h5">
+                                            Categorie
+                                        </Typography>
+
+                                    </CardContent>
+
+                                </Box>
+                                <CardMedia
+                                    component="img"
+                                    sx={{ width: 151 }}
+                                    image="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+                                    alt="Live from space album cover"
+                                />
+                            </Card>
+                        </Item>
+                    </Grid>
+                    <Grid xs={4}>
+                        <Item>         <Card sx={{ display: 'flex' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        Categorie
+                                    </Typography>
+
+                                </CardContent>
+
+                            </Box>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+                                alt="Live from space album cover"
+                            />
+                        </Card></Item>
+                    </Grid>
+                    <Grid xs={4}>
+                        <Item>         <Card sx={{ display: 'flex' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        Categorie
+                                    </Typography>
+
+                                </CardContent>
+
+                            </Box>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+                                alt="Live from space album cover"
+                            />
+                        </Card></Item>
+                    </Grid>
+                    <Grid xs={4}>
+                        <Item>         <Card sx={{ display: 'flex' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        Categorie
+                                    </Typography>
+
+                                </CardContent>
+
+                            </Box>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+                                alt="Live from space album cover"
+                            />
+                        </Card></Item>
+                    </Grid>
+                    <Grid xs={4}>
+                        <Item>         <Card sx={{ display: 'flex' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        Categorie
+                                    </Typography>
+
+                                </CardContent>
+
+                            </Box>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+                                alt="Live from space album cover"
+                            />
+                        </Card></Item>
+                    </Grid>
+                </Grid>
+            </Box> */}
 
             <CategorieTop categories={categories} />
 
